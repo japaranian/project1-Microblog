88b243f41496a5ab45289a283d73fdaacca5876e
japaranian <mina.talai@gmail.com>
2015-01-06 10:14:02 -0500

    project update

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,8 +1,11 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(
-	:adapter => "postgresql",
-	:host => "localhost",
-	:username => "Japaranian",
-	:database => "travel_blog"
-	)
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/travel_blog')
+
+# ActiveRecord::Base.establish_connection(
+# 	:adapter => "postgresql",
+# 	:host => "localhost",
+# 	:username => "Japaranian",
+# 	:database => "travel_blog"
+
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
