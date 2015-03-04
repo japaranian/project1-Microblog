f51b35f0334455887401f086cfd72986de8f877a
japaranian <mina.talai@gmail.com>
2015-03-04 15:05:41 -0500

    changes yo'

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users(id serial primary key, name varchar(50), email varchar(100));
+CREATE TABLE users(id serial primary key, fname varchar(50), lname varchar(50), email varchar(100), password varchar(50));
 
 CREATE TABLE microposts(id serial primary key, title varchar(50), post text, user_id integer, image_url text, tag text, comment_id integer, time_stamp date );
 
